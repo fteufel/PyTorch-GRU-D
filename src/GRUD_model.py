0e82f11b41d4fbb9591dd2b4695b20799fcde41b
fteufel <felix.teufel@gmail.com>
2020-01-02 18:17:25 +0100

    fixed bugs

--- src/GRUD_model.py
+++ src/GRUD_model.py
@@ -34,26 +34,33 @@ class grud_model(torch.nn.Module):
      bias =True, batch_first = False, bidirectional = False, dropout_type ='mloss', dropout = 0):
         super(grud_model, self).__init__()
 
-        self.gru_d = GRUD_cell(input_size = input_size, hidden_size= hidden_size, output_size=input_size, 
-                dropout=dropout, dropout_type=dropout_type, x_mean=x_mean, num_layers=num_layers, return_hidden = True)
+        self.gru_d = GRUD_cell(input_size = input_size, hidden_size= hidden_size, output_size=output_size, 
+                dropout=dropout, dropout_type=dropout_type, x_mean=x_mean)
         self.hidden_to_output = torch.nn.Linear(hidden_size, output_size, bias=True)
+        self.num_layers = num_layers
 
         if self.num_layers >1:
             #(batch, seq, feature)
-            self.gru_layers = nn.gru(input_size = hidden_size, hidden_size = hidden_size, batch_first = True, num_layers = self.num_layers -1)
+            self.gru_layers = torch.nn.GRU(input_size = hidden_size, hidden_size = hidden_size, batch_first = True, num_layers = self.num_layers -1)
 
     def forward(self,input):
 
         #pass through GRU-D
-        output, gru_d_hidden_states = self.gru_d(input)
+        output, hidden = self.gru_d(input)
+        #print(self.gru_d.return_hidden)
+        #output = self.gru_d(input)
+        #print(output.size())
 
         # batch_size, n_hidden, n_timesteps
 
         if self.num_layers >1:
-            output, hidden = self.gru_layers(gru_d_hidden_states)
-    
-        output = torch.nn.Sigmoid(self.hidden_to_output(hidden))
+            output, hidden = self.gru_layers(hidden)
+  
 
+            output = self.hidden_to_output(output)
+            output = torch.sigmoid(output)
+
+        print("final output size passed as model result")
         print(output.size())
         return output
 
